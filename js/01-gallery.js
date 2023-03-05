2d0efec152b290a24b9c3933e6671d9ed716ee1d
Nearh777 <nearh777@gmail.com>
2023-03-05 10:10:34 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,4 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+// Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
