b1ab53c6107b39736dfd92e49bca6494afc6d2b8
Nearh777 <nearh777@gmail.com>
2023-03-05 11:47:04 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,7 +2,7 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 // //1. Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
 // const listEl = document.querySelector('.gallery');
-// const makeGalleryEl = ({preview, original, description})  => 
+// const makeGalleryEl = ({preview, original, description})  =>
 // `<li class='gallery-item'>
 // <img src='${preview}' alt='${description}'>
 // </li>`
@@ -13,11 +13,11 @@ import { galleryItems } from './gallery-items.js';
 
 // console.log(galleryItems);
 
-
 const galleryContainer = document.querySelector('.gallery');
 function createGalleryCardsMarkup(items) {
-    return items.map(({ preview, original, description }) => {
-        return `
+  return items
+    .map(({ preview, original, description }) => {
+      return `
     <div class="gallery__item">
         <a class="gallery__link" href="${original}">
         <img
@@ -28,22 +28,25 @@ function createGalleryCardsMarkup(items) {
         </a>
     </div>
     `;
-    }).join('');
-};
+    })
+    .join('');
+}
 const cardsMarkup = createGalleryCardsMarkup(galleryItems);
 galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
 
-galleryContainer.addEventListener(click, hendleGalleryClick)
+galleryContainer.addEventListener(click, hendleGalleryClick);
 
-function hendleGalleryClick (e) {
-    e.preventDefault()
-    if (e.targett.nodeName !== 'IMG') {
-        return
-    }
+function hendleGalleryClick(e) {
+  e.preventDefault();
+  if (e.targett.nodeName !== 'IMG') {
+    return;
+  }
 
-    const modalImg = e.targett.dataset.source
-    console.log(modalImg);
+  const modalImg = e.targett.dataset.source;
+  console.log(modalImg);
 
-    const instance = bacicLightbox.create(`<img src="${modalImg}`)
+  const instance = bacicLightbox.create(
+    `<img src="${modalImg}" 
+    width="800" height="600">`)
+    instance.show()
 }
-
